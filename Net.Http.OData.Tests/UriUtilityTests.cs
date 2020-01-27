2518d898288b9c39512fa8c710cbda957ba0cfcb
Trevor Pilley <TrevorPilley@users.noreply.github.com>
2020-01-27 09:29:43 +0000

    Added Uri utility methods for returning the @odata.context

--- Net.Http.OData.Tests/UriUtilityTests.cs
+++ Net.Http.OData.Tests/UriUtilityTests.cs
@@ -1,32 +1,202 @@
-﻿using Xunit;
+﻿using Net.Http.OData.Model;
+using Net.Http.OData.Query;
+using Xunit;
 
 namespace Net.Http.OData.Tests
 {
     public class UriUtilityTests
     {
-        [Theory]
-        [InlineData("/odata/Products")]
-        [InlineData("/OData/Products")]
-        [InlineData("/OData/Products/")]
-        [InlineData("/OData/Products(1)")]
-        [InlineData("/OData/Products(1)/Name/$value")]
-        [InlineData("/OData/Products%281%29")]
-        [InlineData("/OData/Products$select=Name")]
-        public void ResolveEntitySetName_ReturnsEntitySetName(string path)
+        [Fact]
+        public void ODataServiceContextUri()
         {
-            string entitySetName = UriUtility.ResolveEntitySetName(path);
+            Assert.Equal(
+                "https://services.odata.org/OData/$metadata",
+                UriUtility.ODataServiceContextUri(ODataMetadataLevel.Full, "https", "services.odata.org", "/OData/Products").ToString());
 
-            Assert.Equal("Products", entitySetName);
+            Assert.Equal(
+                "https://services.odata.org/OData/$metadata",
+                UriUtility.ODataServiceContextUri(ODataMetadataLevel.Minimal, "https", "services.odata.org", "/OData/Products").ToString());
+
+            Assert.Equal(
+                "https://services.odata.org/OData/$metadata",
+                UriUtility.ODataServiceContextUri(ODataMetadataLevel.Minimal, "https", "services.odata.org", "/OData/Products/").ToString());
+
+            Assert.Equal(
+                "https://services.odata.org/OData/$metadata",
+                UriUtility.ODataServiceContextUri(ODataMetadataLevel.Minimal, "https", "services.odata.org", "/OData/Products(1)").ToString());
+
+            Assert.Equal(
+                "https://services.odata.org/OData/$metadata",
+                UriUtility.ODataServiceContextUri(ODataMetadataLevel.Minimal, "https", "services.odata.org", "/OData/Products(1)/Name/$value").ToString());
+
+            Assert.Equal(
+                "https://services.odata.org/OData/$metadata",
+                UriUtility.ODataServiceContextUri(ODataMetadataLevel.Minimal, "https", "services.odata.org", "/OData/Products%281%29").ToString());
+
+            Assert.Equal(
+                "https://services.odata.org/OData/$metadata",
+                UriUtility.ODataServiceContextUri(ODataMetadataLevel.Minimal, "https", "services.odata.org", "/OData/Products$select=Name").ToString());
+
+            Assert.Null(UriUtility.ODataServiceContextUri(ODataMetadataLevel.None, "https", "services.odata.org", "/OData/Products"));
         }
 
-        [Theory]
-        [InlineData("/odata")]
-        [InlineData("/api/Products")]
-        public void ResolveEntitySetName_ReturnsNull(string path)
+        [Fact]
+        public void ODataServiceContextUri_WithEntitySet()
         {
-            string entitySetName = UriUtility.ResolveEntitySetName(path);
+            TestHelper.EnsureEDM();
 
-            Assert.Null(entitySetName);
+            EntitySet entitySet = EntityDataModel.Current.EntitySets["Products"];
+
+            Assert.Equal(
+                "https://services.odata.org/OData/$metadata#Products",
+                UriUtility.ODataServiceContextUri(ODataMetadataLevel.Full, "https", "services.odata.org", "/OData/Products/", entitySet).ToString());
+
+            Assert.Equal(
+                "https://services.odata.org/OData/$metadata#Products",
+                UriUtility.ODataServiceContextUri(ODataMetadataLevel.Minimal, "https", "services.odata.org", "/OData/Products/", entitySet).ToString());
+
+            Assert.Null(UriUtility.ODataServiceContextUri(ODataMetadataLevel.None, "https", "services.odata.org", "/OData/Products", entitySet));
+        }
+
+        [Fact]
+        public void ODataServiceContextUri_WithEntitySet_AndSelectProperties()
+        {
+            TestHelper.EnsureEDM();
+
+            EntitySet entitySet = EntityDataModel.Current.EntitySets["Products"];
+
+            var queryOptions = new ODataQueryOptions("?$select=Name,Description,Price", entitySet);
+
+            Assert.Equal(
+                "https://services.odata.org/OData/$metadata#Products(Name,Description,Price)",
+                UriUtility.ODataServiceContextUri(ODataMetadataLevel.Full, "https", "services.odata.org", "/OData/Products/", entitySet, queryOptions.Select).ToString());
+
+            Assert.Equal(
+                "https://services.odata.org/OData/$metadata#Products(Name,Description,Price)",
+                UriUtility.ODataServiceContextUri(ODataMetadataLevel.Minimal, "https", "services.odata.org", "/OData/Products/", entitySet, queryOptions.Select).ToString());
+
+            Assert.Null(UriUtility.ODataServiceContextUri(ODataMetadataLevel.None, "https", "services.odata.org", "/OData/Products/", entitySet, queryOptions.Select));
+        }
+
+        [Fact]
+        public void ODataServiceContextUri_WithEntitySet_AndSelectStar()
+        {
+            TestHelper.EnsureEDM();
+
+            EntitySet entitySet = EntityDataModel.Current.EntitySets["Products"];
+
+            var queryOptions = new ODataQueryOptions("?$select=*", entitySet);
+
+            Assert.Equal(
+                "https://services.odata.org/OData/$metadata#Products(*)",
+                UriUtility.ODataServiceContextUri(ODataMetadataLevel.Full, "https", "services.odata.org", "/OData/Products/", entitySet, queryOptions.Select).ToString());
+
+            Assert.Equal(
+                "https://services.odata.org/OData/$metadata#Products(*)",
+                UriUtility.ODataServiceContextUri(ODataMetadataLevel.Minimal, "https", "services.odata.org", "/OData/Products/", entitySet, queryOptions.Select).ToString());
+
+            Assert.Null(UriUtility.ODataServiceContextUri(ODataMetadataLevel.None, "https", "services.odata.org", "/OData/Products/", entitySet, queryOptions.Select));
+        }
+
+        [Fact]
+        public void ODataServiceContextUri_WithEntitySet_EntityKey()
+        {
+            TestHelper.EnsureEDM();
+
+            EntitySet entitySet = EntityDataModel.Current.EntitySets["Products"];
+
+            Assert.Equal(
+                "https://services.odata.org/OData/$metadata#Products/$entity",
+                UriUtility.ODataServiceContextUri(ODataMetadataLevel.Full, "https", "services.odata.org", "/OData/Products/", entitySet, 10142276).ToString());
+
+            Assert.Equal(
+                "https://services.odata.org/OData/$metadata#Products/$entity",
+                UriUtility.ODataServiceContextUri(ODataMetadataLevel.Minimal, "https", "services.odata.org", "/OData/Products/", entitySet, 10142276).ToString());
+
+            Assert.Null(UriUtility.ODataServiceContextUri(ODataMetadataLevel.None, "https", "services.odata.org", "/OData/Products/", entitySet, 10142276));
+        }
+
+        [Fact]
+        public void ODataServiceContextUri_WithEntitySet_EntityKeyInt_Property()
+        {
+            TestHelper.EnsureEDM();
+
+            EntitySet entitySet = EntityDataModel.Current.EntitySets["Products"];
+
+            Assert.Equal(
+                "https://services.odata.org/OData/$metadata#Products(10142276)/Name",
+                UriUtility.ODataServiceContextUri(ODataMetadataLevel.Full, "https", "services.odata.org", "/OData/Products/", entitySet, 10142276, "Name").ToString());
+
+            Assert.Equal(
+                "https://services.odata.org/OData/$metadata#Products(10142276)/Name",
+                UriUtility.ODataServiceContextUri(ODataMetadataLevel.Minimal, "https", "services.odata.org", "/OData/Products/", entitySet, 10142276, "Name").ToString());
+
+            Assert.Null(UriUtility.ODataServiceContextUri(ODataMetadataLevel.None, "https", "services.odata.org", "/OData/Products/", entitySet, 10142276, "Name"));
+        }
+
+        [Fact]
+        public void ODataServiceContextUri_WithEntitySet_EntityKeyString_Property()
+        {
+            TestHelper.EnsureEDM();
+
+            EntitySet entitySet = EntityDataModel.Current.EntitySets["Customers"];
+
+            Assert.Equal(
+                "https://services.odata.org/OData/$metadata#Customers('Tesco')/CompanyName",
+                UriUtility.ODataServiceContextUri(ODataMetadataLevel.Full, "https", "services.odata.org", "/OData/Customers/", entitySet, "Tesco", "CompanyName").ToString());
+
+            Assert.Equal(
+                "https://services.odata.org/OData/$metadata#Customers('Tesco')/CompanyName",
+                UriUtility.ODataServiceContextUri(ODataMetadataLevel.Minimal, "https", "services.odata.org", "/OData/Customers/", entitySet, "Tesco", "CompanyName").ToString());
+
+            Assert.Null(UriUtility.ODataServiceContextUri(ODataMetadataLevel.None, "https", "services.odata.org", "/OData/Customers/", entitySet, "Tesco", "CompanyName"));
+        }
+
+        [Fact]
+        public void ODataServiceRootUri()
+        {
+            Assert.Equal(
+                "https://services.odata.org/OData/",
+                UriUtility.ODataServiceRootUri("https", "services.odata.org", "/OData/Products").ToString());
+
+            Assert.Equal(
+                "https://services.odata.org/OData/",
+                UriUtility.ODataServiceRootUri("https", "services.odata.org", "/OData/Products/").ToString());
+
+            Assert.Equal(
+                "https://services.odata.org/OData/",
+                UriUtility.ODataServiceRootUri("https", "services.odata.org", "/OData/Products(1)").ToString());
+
+            Assert.Equal(
+                "https://services.odata.org/OData/",
+                UriUtility.ODataServiceRootUri("https", "services.odata.org", "/OData/Products(1)/Name/$value").ToString());
+
+            Assert.Equal(
+                "https://services.odata.org/OData/",
+                UriUtility.ODataServiceRootUri("https", "services.odata.org", "/OData/Products%281%29").ToString());
+
+            Assert.Equal(
+                "https://services.odata.org/OData/",
+                UriUtility.ODataServiceRootUri("https", "services.odata.org", "/OData/Products$select=Name").ToString());
+        }
+
+        [Fact]
+        public void ResolveEntitySetName_ReturnsEntitySetName_IfODataPath()
+        {
+            Assert.Equal("Products", UriUtility.ResolveEntitySetName("/odata/Products"));
+            Assert.Equal("Products", UriUtility.ResolveEntitySetName("/OData/Products"));
+            Assert.Equal("Products", UriUtility.ResolveEntitySetName("/OData/Products/"));
+            Assert.Equal("Products", UriUtility.ResolveEntitySetName("/OData/Products(1)"));
+            Assert.Equal("Products", UriUtility.ResolveEntitySetName("/OData/Products(1)/Name/$value"));
+            Assert.Equal("Products", UriUtility.ResolveEntitySetName("/OData/Products%281%29"));
+            Assert.Equal("Products", UriUtility.ResolveEntitySetName("/OData/Products$select=Name"));
+        }
+
+        [Fact]
+        public void ResolveEntitySetName_ReturnsNull_IfNotODataPath()
+        {
+            Assert.Null(UriUtility.ResolveEntitySetName("/odata"));
+            Assert.Null(UriUtility.ResolveEntitySetName("/api/Products"));
         }
     }
 }
--- Net.Http.OData/UriUtility.cs
+++ Net.Http.OData/UriUtility.cs
@@ -11,6 +11,11 @@
 // </copyright>
 // -----------------------------------------------------------------------
 using System;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using Net.Http.OData.Model;
+using Net.Http.OData.Query;
 
 namespace Net.Http.OData
 {
@@ -19,7 +24,83 @@ namespace Net.Http.OData
     /// </summary>
     public static class UriUtility
     {
+        private const string SchemeDelimiter = "://";
         private static readonly char[] s_nonNameCharacters = new[] { '(', '/', '$', '%' };
+
+        /// <summary>
+        /// Returns a <see cref="Uri"/> which represents the @odata.context URI.
+        /// </summary>
+        /// <param name="metadataLevel">The OData metadata level for the current request.</param>
+        /// <param name="scheme">The scheme, which should be HTTP or HTTPS.</param>
+        /// <param name="host">The host (e.g. odataservice.org).</param>
+        /// <param name="path">The path (e.g. /odata/...)</param>
+        /// <returns>A <see cref="Uri"/> which represents the @odata.context URI.</returns>
+        public static Uri ODataServiceContextUri(ODataMetadataLevel metadataLevel, string scheme, string host, string path)
+            => metadataLevel != ODataMetadataLevel.None ? new Uri(ODataContextUriBuilder(scheme, host, path).ToString()) : default;
+
+        /// <summary>
+        /// Returns a <see cref="Uri"/> which represents the @odata.context URI.
+        /// </summary>
+        /// <param name="metadataLevel">The OData metadata level for the current request.</param>
+        /// <param name="scheme">The scheme, which should be HTTP or HTTPS.</param>
+        /// <param name="host">The host (e.g. odataservice.org).</param>
+        /// <param name="path">The path (e.g. /odata/...)</param>
+        /// <param name="entitySet">The <see cref="EntitySet"/>.</param>
+        /// <returns>A <see cref="Uri"/> which represents the @odata.context URI.</returns>
+        public static Uri ODataServiceContextUri(ODataMetadataLevel metadataLevel, string scheme, string host, string path, EntitySet entitySet)
+            => metadataLevel != ODataMetadataLevel.None ? new Uri(ODataContextUriBuilder(scheme, host, path, entitySet).ToString()) : default;
+
+        /// <summary>
+        /// Returns a <see cref="Uri"/> which represents the @odata.context URI.
+        /// </summary>
+        /// <param name="metadataLevel">The OData metadata level for the current request.</param>
+        /// <param name="scheme">The scheme, which should be HTTP or HTTPS.</param>
+        /// <param name="host">The host (e.g. odataservice.org).</param>
+        /// <param name="path">The path (e.g. /odata/...)</param>
+        /// <param name="entitySet">The <see cref="EntitySet"/>.</param>
+        /// <param name="selectQueryOption">The $select query option.</param>
+        /// <returns>A <see cref="Uri"/> which represents the @odata.context URI.</returns>
+        public static Uri ODataServiceContextUri(ODataMetadataLevel metadataLevel, string scheme, string host, string path, EntitySet entitySet, SelectExpandQueryOption selectQueryOption)
+            => metadataLevel != ODataMetadataLevel.None ? new Uri(ODataContextUriBuilder(scheme, host, path, entitySet, selectQueryOption).ToString()) : default;
+
+        /// <summary>
+        /// Returns a <see cref="Uri"/> which represents the @odata.context URI.
+        /// </summary>
+        /// <param name="metadataLevel">The OData metadata level for the current request.</param>
+        /// <param name="scheme">The scheme, which should be HTTP or HTTPS.</param>
+        /// <param name="host">The host (e.g. odataservice.org).</param>
+        /// <param name="path">The path (e.g. /odata/...)</param>
+        /// <param name="entitySet">The <see cref="EntitySet"/>.</param>
+        /// <param name="entityKey">The entity key for the entity set.</param>
+        /// <typeparam name="TEntityKey">The type of the entity key.</typeparam>
+        /// <returns>A <see cref="Uri"/> which represents the @odata.context URI.</returns>
+        public static Uri ODataServiceContextUri<TEntityKey>(ODataMetadataLevel metadataLevel, string scheme, string host, string path, EntitySet entitySet, TEntityKey entityKey)
+            => metadataLevel != ODataMetadataLevel.None ? new Uri(ODataContextUriBuilder(scheme, host, path, entitySet).Append("/$entity").ToString()) : default;
+
+        /// <summary>
+        /// Returns a <see cref="Uri"/> which represents the @odata.context URI.
+        /// </summary>
+        /// <param name="metadataLevel">The OData metadata level for the current request.</param>
+        /// <param name="scheme">The scheme, which should be HTTP or HTTPS.</param>
+        /// <param name="host">The host (e.g. odataservice.org).</param>
+        /// <param name="path">The path (e.g. /odata/...)</param>
+        /// <param name="entitySet">The <see cref="EntitySet"/>.</param>
+        /// <param name="entityKey">The entity key for the entity set.</param>
+        /// <param name="propertyName">The property name.</param>
+        /// <typeparam name="TEntityKey">The type of the entity key.</typeparam>
+        /// <returns>A <see cref="Uri"/> which represents the @odata.context URI.</returns>
+        public static Uri ODataServiceContextUri<TEntityKey>(ODataMetadataLevel metadataLevel, string scheme, string host, string path, EntitySet entitySet, TEntityKey entityKey, string propertyName)
+            => metadataLevel != ODataMetadataLevel.None ? new Uri(ODataContextUriBuilder(scheme, host, path, entitySet, entityKey, propertyName).ToString()) : default;
+
+        /// <summary>
+        /// Returns a <see cref="Uri"/> which represents the root of the OData service.
+        /// </summary>
+        /// <param name="scheme">The scheme, which should be HTTP or HTTPS.</param>
+        /// <param name="host">The host (e.g. odataservice.org).</param>
+        /// <param name="path">The path (e.g. /odata/...)</param>
+        /// <returns>A <see cref="Uri"/> which represents the root of the OData service.</returns>
+        public static Uri ODataServiceRootUri(string scheme, string host, string path)
+            => new Uri(ODataServiceUriBuilder(scheme, host, path).ToString());
 
         /// <summary>
         /// Resolves the entity set name from the specified path segment of a URI.
@@ -55,6 +136,93 @@ namespace Net.Http.OData
             }
 
             return null;
+        }
+
+        private static StringBuilder ODataContextUriBuilder(string scheme, string host, string path)
+            => ODataServiceUriBuilder(scheme, host, path).Append("$metadata");
+
+        private static StringBuilder ODataContextUriBuilder(string scheme, string host, string path, EntitySet entitySet)
+        {
+            if (entitySet is null)
+            {
+                throw new ArgumentNullException(nameof(entitySet));
+            }
+
+            return ODataContextUriBuilder(scheme, host, path).Append('#').Append(entitySet.Name);
+        }
+
+        private static StringBuilder ODataContextUriBuilder(string scheme, string host, string path, EntitySet entitySet, SelectExpandQueryOption selectQueryOption)
+        {
+            StringBuilder contextUriBuilder = ODataContextUriBuilder(scheme, host, path, entitySet);
+
+            if (selectQueryOption?.RawValue.Equals("$select=*", StringComparison.Ordinal) == true)
+            {
+                contextUriBuilder.Append("(*)");
+            }
+            else if (selectQueryOption?.PropertyPaths.Count > 0)
+            {
+                contextUriBuilder.AppendFormat(CultureInfo.InvariantCulture, "({0})", string.Join(",", selectQueryOption.PropertyPaths.Select(p => p.Property.Name)));
+            }
+
+            return contextUriBuilder;
+        }
+
+        private static StringBuilder ODataContextUriBuilder<TEntityKey>(string scheme, string host, string path, EntitySet entitySet, TEntityKey entityKey, string propertyName)
+        {
+            StringBuilder contextUriBuilder = ODataContextUriBuilder(scheme, host, path, entitySet);
+
+            if (typeof(TEntityKey) == typeof(string))
+            {
+                contextUriBuilder.Append("('").Append(entityKey.ToString()).Append("')");
+            }
+            else
+            {
+                contextUriBuilder.Append("(").Append(entityKey.ToString()).Append(")");
+            }
+
+            contextUriBuilder.Append('/').Append(propertyName);
+
+            return contextUriBuilder;
+        }
+
+        private static StringBuilder ODataServiceUriBuilder(string scheme, string host, string path)
+        {
+            if (string.IsNullOrWhiteSpace(scheme))
+            {
+                throw new ArgumentException("Scheme must be specified", nameof(scheme));
+            }
+
+            if (string.IsNullOrWhiteSpace(host))
+            {
+                throw new ArgumentException("Host must be specified", nameof(host));
+            }
+
+            if (string.IsNullOrWhiteSpace(path))
+            {
+                throw new ArgumentException("Path must be specified", nameof(path));
+            }
+
+            // Adding 5 for the length of the word 'odata'.
+            int odataEndIndex = path.IndexOf("odata", StringComparison.OrdinalIgnoreCase) + 5;
+
+            // PERF: Calculate string length to allocate correct buffer size for StringBuilder, adding 1 for trailing slash.
+            int length = scheme.Length + SchemeDelimiter.Length + host.Length + odataEndIndex + 1;
+
+            StringBuilder uriBuilder = new StringBuilder(length)
+                  .Append(scheme)
+                  .Append(SchemeDelimiter)
+                  .Append(host);
+
+            // PERF: Append character by character up to the end of 'odata' to avoid the string allocation for using substring.
+            for (int i = 0; i < odataEndIndex; i++)
+            {
+                uriBuilder.Append(path[i]);
+            }
+
+            uriBuilder.Append('/');
+
+            System.Diagnostics.Debug.Assert(uriBuilder.Capacity == length, "The StringBuilder should not increase from the originally specified size");
+            return uriBuilder;
         }
     }
 }
