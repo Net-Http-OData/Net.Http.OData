9cb6f12d28b07f16cd9af1a0a4c7f9aa7b1d3f0f
Trevor Pilley <TrevorPilley@users.noreply.github.com>
2020-01-29 15:29:37 +0000

    Replaced local build batch file with powershell script

--- build.ps1
+++ build.ps1
@@ -1,7 +1,12 @@
-REM A script for building, testing and viewing code coverage locally
+# A script for building, testing and viewing code coverage locally
 dotnet build
+
 dotnet test /p:CollectCoverage=true /p:CoverletOutputFormat=cobertura /p:CoverletOutput=.\TestResults\
-cd Net.Http.OData.Tests
+
+Set-Location Net.Http.OData.Tests
+
 dotnet reportgenerator "-reports:TestResults\coverage.cobertura.xml" "-targetdir:TestResults\html" -reporttypes:HTML;
-start "" "TestResults\html\index.htm"
-cd..
+
+Start-Process "TestResults\html\index.htm"
+
+Set-Location ..
