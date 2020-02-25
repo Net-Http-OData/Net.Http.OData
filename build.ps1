a431450007eab9fad7ffc48c7b630f38d6b6ba6d
Trevor Pilley <TrevorPilley@users.noreply.github.com>
2020-02-25 15:37:43 +0000

    Updated test coverage path

--- build.ps1
+++ build.ps1
@@ -1,12 +1,19 @@
 # A script for building, testing and viewing code coverage locally
+$scriptPath = Split-Path $script:MyInvocation.MyCommand.Path
+$testResults = Join-Path $scriptPath -ChildPath '\Net.Http.OData.Tests\TestResults'
+
+if (Test-Path $testResults){
+    Remove-Item -Path $testResults -Recurse
+}
+
 dotnet clean
 dotnet build
 dotnet test /p:CollectCoverage=true /p:CoverletOutputFormat=cobertura /p:CoverletOutput=.\TestResults\
 
 Set-Location Net.Http.OData.Tests
 
-dotnet reportgenerator "-reports:TestResults\coverage.cobertura.xml" "-targetdir:TestResults\html" -reporttypes:HTML;
+dotnet reportgenerator "-reports:TestResults\coverage.cobertura.xml" "-targetdir:TestResults\Coverage" -reporttypes:HTML;
 
-Start-Process "TestResults\html\index.htm"
+Start-Process "TestResults\Coverage\index.htm"
 
 Set-Location ..
