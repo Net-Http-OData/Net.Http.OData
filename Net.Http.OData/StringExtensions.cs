848b0ed300948086c53a3b60234ea27b9e297042
Trevor Pilley <TrevorPilley@users.noreply.github.com>
2020-02-27 11:37:30 +0000

    Removed unused method

--- Net.Http.OData/StringExtensions.cs
+++ Net.Http.OData/StringExtensions.cs
@@ -20,16 +20,6 @@ namespace Net.Http.OData
     internal static class StringExtensions
     {
         /// <summary>
-        /// Reads the string in slices separated by a character from the start of the string.
-        /// </summary>
-        /// <param name="value">The string to read.</param>
-        /// <param name="separator">The separator to slice by.</param>
-        /// <returns>An IEnumerable containing the slices of the string.</returns>
-        /// <remarks>Saves the extra string allocation that would occur by calling string.Split() by avoiding the string array if the array is not needed.</remarks>
-        internal static IEnumerable<string> Slice(this string value, char separator)
-            => Slice(value, separator, 0);
-
-        /// <summary>
         /// Reads the string in slices separated by a character from the specified position within the string.
         /// </summary>
         /// <param name="value">The string to read.</param>
