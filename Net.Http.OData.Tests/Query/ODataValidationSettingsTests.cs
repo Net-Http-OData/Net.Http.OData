cf5f78e8d1bdcafdb73028821bb1d360b1eeffc6
Trevor Pilley <TrevorPilley@users.noreply.github.com>
2020-02-27 11:36:34 +0000

    Update ODataValidationSettingsTests.cs

--- Net.Http.OData.Tests/Query/ODataValidationSettingsTests.cs
+++ Net.Http.OData.Tests/Query/ODataValidationSettingsTests.cs
@@ -5,39 +5,53 @@ namespace Net.Http.OData.Tests.Query
 {
     public class ODataValidationSettingsTests
     {
+        [Fact]
+        public void Equals_ReturnsFalse_ForDifferentValue()
+        {
+            Assert.False(ODataValidationSettings.None.Equals(ODataValidationSettings.All));
+            Assert.False(ODataValidationSettings.None.Equals(default));
+            Assert.False(ODataValidationSettings.None.Equals("foo"));
+
+            Assert.False(ODataValidationSettings.None == ODataValidationSettings.All);
+            Assert.False(ODataValidationSettings.None == default);
+        }
+
+        [Fact]
+        public void Equals_ReturnsTrue_ForSameInstance()
+        {
+            Assert.True(ODataValidationSettings.None.Equals(ODataValidationSettings.None));
+            Assert.True(ODataValidationSettings.All.Equals(ODataValidationSettings.All));
+
+            Assert.True(ODataValidationSettings.None == ODataValidationSettings.None);
+            Assert.True(ODataValidationSettings.All == ODataValidationSettings.All);
+
+            Assert.True(default(ODataValidationSettings) == default);
+        }
+
+        [Fact]
+        public void NotEquals_ReturnsFalse_ForSameValue() => Assert.False(ODataValidationSettings.None != ODataValidationSettings.None);
+
+        [Fact]
+        public void NotEquals_ReturnsTrue_ForDifferentValue() => Assert.True(ODataValidationSettings.None != ODataValidationSettings.All);
+
         public class All
         {
             private readonly ODataValidationSettings _all = ODataValidationSettings.All;
 
             [Fact]
-            public void SetsAllowedArithmeticOperatorsToAll()
-            {
-                Assert.Equal(AllowedArithmeticOperators.All, _all.AllowedArithmeticOperators);
-            }
+            public void SetsAllowedArithmeticOperatorsToAll() => Assert.Equal(AllowedArithmeticOperators.All, _all.AllowedArithmeticOperators);
 
             [Fact]
-            public void SetsAllowedFunctionsTAllFunctions()
-            {
-                Assert.Equal(AllowedFunctions.AllFunctions, _all.AllowedFunctions);
-            }
+            public void SetsAllowedFunctionsTAllFunctions() => Assert.Equal(AllowedFunctions.AllFunctions, _all.AllowedFunctions);
 
             [Fact]
-            public void SetsAllowedLogicalOperatorsToAll()
-            {
-                Assert.Equal(AllowedLogicalOperators.All, _all.AllowedLogicalOperators);
-            }
+            public void SetsAllowedLogicalOperatorsToAll() => Assert.Equal(AllowedLogicalOperators.All, _all.AllowedLogicalOperators);
 
             [Fact]
-            public void SetsAllowedQueryOptionsToAll()
-            {
-                Assert.Equal(AllowedQueryOptions.All, _all.AllowedQueryOptions);
-            }
+            public void SetsAllowedQueryOptionsToAll() => Assert.Equal(AllowedQueryOptions.All, _all.AllowedQueryOptions);
 
             [Fact]
-            public void SetsMaxTopToOneHundred()
-            {
-                Assert.Equal(100, _all.MaxTop);
-            }
+            public void SetsMaxTopToOneHundred() => Assert.Equal(100, _all.MaxTop);
         }
 
         public class None
@@ -45,34 +59,19 @@ namespace Net.Http.OData.Tests.Query
             private readonly ODataValidationSettings _none = ODataValidationSettings.None;
 
             [Fact]
-            public void SetsAllowedArithmeticOperatorsToNone()
-            {
-                Assert.Equal(AllowedArithmeticOperators.None, _none.AllowedArithmeticOperators);
-            }
+            public void SetsAllowedArithmeticOperatorsToNone() => Assert.Equal(AllowedArithmeticOperators.None, _none.AllowedArithmeticOperators);
 
             [Fact]
-            public void SetsAllowedFunctionsToNone()
-            {
-                Assert.Equal(AllowedFunctions.None, _none.AllowedFunctions);
-            }
+            public void SetsAllowedFunctionsToNone() => Assert.Equal(AllowedFunctions.None, _none.AllowedFunctions);
 
             [Fact]
-            public void SetsAllowedLogicalOperatorsToNone()
-            {
-                Assert.Equal(AllowedLogicalOperators.None, _none.AllowedLogicalOperators);
-            }
+            public void SetsAllowedLogicalOperatorsToNone() => Assert.Equal(AllowedLogicalOperators.None, _none.AllowedLogicalOperators);
 
             [Fact]
-            public void SetsAllowedQueryOptionsToNone()
-            {
-                Assert.Equal(AllowedQueryOptions.None, _none.AllowedQueryOptions);
-            }
+            public void SetsAllowedQueryOptionsToNone() => Assert.Equal(AllowedQueryOptions.None, _none.AllowedQueryOptions);
 
             [Fact]
-            public void SetsMaxTopToZero()
-            {
-                Assert.Equal(0, _none.MaxTop);
-            }
+            public void SetsMaxTopToZero() => Assert.Equal(0, _none.MaxTop);
         }
     }
 }
