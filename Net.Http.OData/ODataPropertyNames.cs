59f14398480d30320b53a17706d5a4f8b02b6100
Trevor Pilley <TrevorPilley@users.noreply.github.com>
2020-04-06 12:04:39 +0100

    Added ODataPropertyNames constants

---
+++ Net.Http.OData/ODataPropertyNames.cs
@@ -0,0 +1,35 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="ODataPropertyNames.cs" company="Project Contributors">
+// Copyright Project Contributors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// </copyright>
+// -----------------------------------------------------------------------
+namespace Net.Http.OData
+{
+    /// <summary>
+    /// The property names for OData responses.
+    /// </summary>
+    public static class ODataPropertyNames
+    {
+        /// <summary>
+        /// The @odata.context property.
+        /// </summary>
+        public const string ODataContext = "@odata.context";
+
+        /// <summary>
+        /// The @odata.count property.
+        /// </summary>
+        public const string ODataCount = "@odata.count";
+
+        /// <summary>
+        /// The @odata.nextLink property.
+        /// </summary>
+        public const string ODataNextLink = "@odata.nextLink";
+    }
+}
